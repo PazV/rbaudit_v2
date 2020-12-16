ff1de4c7c965467175e43012a0e8f34dc462c377
Paz Garcia <pgarcia@russellbedford.mx>
2020-12-16 08:42:27 -0600

    Ajustar tamaño de íconos y texto de left side bar

--- static/js/activities.js
+++ static/js/activities.js
@@ -1,8 +1,12 @@
 $(document).ready( function () {
-    $('#table_id').DataTable();
-    $('#table_id').DataTable({
+
+    var date = new Date();
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString().split("T")[0];
+    $("#activ_date").val(lastDay);
+
+    $('#tableActList').DataTable({
         searching: false,
         ordering:  false,
         lengthChange:false
     });
-} );
+});
