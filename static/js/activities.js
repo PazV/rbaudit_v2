9f4b60cd310b01a7c1e4b3b75db9e7f9cc9b6b3f
Paz Garcia <pgarcia@russellbedford.mx>
2020-12-17 16:46:44 -0600

    Mostrar actividades

--- static/js/activities.js
+++ static/js/activities.js
@@ -1,12 +1,40 @@
-$(document).ready( function () {
-
+$(document).ready(function(){
+    var me = this;
+    this.user_info=JSON.parse($("#spnSession")[0].textContent);
     var date = new Date();
     var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString().split("T")[0];
     $("#activ_date").val(lastDay);
+    $("#tableActList").DataTable();
+    loadUserActivities(me.user_info.user_id,lastDay);
 
-    $('#tableActList').DataTable({
-        searching: false,
-        ordering:  false,
-        lengthChange:false
+    $("#activ_date").on('change',function(x){
+        // console.log(x.currentTarget.value);
+        loadUserActivities(me.user_info,x.currentTarget.value);
     });
 });
+
+function loadUserActivities(user_id,date){
+    $("#tableActList").DataTable({
+        "scrollY":"300px",
+        "scrollCollapse":true,
+        "lengthChange":false,
+        serverSide:true,
+        destroy:true,
+        searching:false,
+        ordering:false,
+        ajax:{
+            data:{'user_id':user_id,'date':date},
+            url:'/activity-list/getUserActivities',
+            dataSrc:'data',
+            type:'POST',
+            error:ajaxError,
+        },
+        columns:[
+            {data:'form_name',"width":"35%", "className":"dt-head-center dt-body-left"},
+            {data:'project_name',"width":"35%", "className":"dt-head-center dt-body-left"},
+            {data:'resolve_before',"width":"10%", "className":"dt-center"},
+            {data:'status',"width":"15%", "className":"dt-head-center dt-body-left"},
+            {data:'link',"width":"5%", "className":"dt-center"}
+        ]
+    })
+}
--- views/activities.py
+++ views/activities.py
@@ -40,3 +40,57 @@ def activityList():
     g.profile_picture_class=user_info['profile_picture_class']
     g.notifications=False
     return render_template('activity_list.html',g=g)
+
+@bp.route('/getUserActivities',methods=['GET','POST'])
+@is_logged_in
+def getUserActivities():
+    #obtener las actividades pendientes del usuario
+    response={}
+    try:
+        if request.method=='POST':
+
+            user_id=int(request.form['user_id'])
+
+            date=request.form['date']
+            start=int(request.form['start'])
+            limit=int(request.form['length'])
+
+            forms=db.query("""
+                select a.form_id, a.project_id, a.name as form_name, to_char(a.resolve_before, 'DD/MM/YYYY') as resolve_before,
+                c.name || ' - ' || c.company_name as project_name,
+                case when a.resolve_before between current_date and current_date + interval '3 days' then 'act-priority-orange' when a.resolve_before < current_date then 'act-priority-red' when a.resolve_before > current_date + interval '3 days' then 'act-priority-yellow' end as priority_class,
+                case when b.status = 'Publicado' then 'Sin iniciar' else b.status end as status
+                from project.form a, project.form_status b, project.project c
+                where a.project_id in (select project_id from project.project where (manager=%s or partner=%s or assigned_to=%s or %s in (select c.user_id from project.form_revisions c where c.form_id=a.form_id)) and now() >= start_date and now() <= finish_date) and a.project_id=c.project_id and a.status_id=b.status_id
+                and a.status_id in (3,4,5,6) and resolve_before <= '%s'
+                --order by expired desc, a.resolve_before asc
+                offset %s limit %s
+            """%(user_id,user_id,user_id,user_id,date,start,limit)).dictresult()
+
+            form_count=db.query("""
+                select count(a.form_id)
+                from project.form a, project.form_status b, project.project c
+                where a.project_id in (select project_id from project.project where (manager=%s or partner=%s or assigned_to=%s or %s in (select c.user_id from project.form_revisions c where c.form_id=a.form_id)) and now() >= start_date and now() <= finish_date) and a.project_id=c.project_id and a.status_id=b.status_id
+                and a.status_id in (3,4,5,6) and resolve_before <= '%s'
+            """%(user_id,user_id,user_id,user_id,date)).dictresult()
+
+            for f in forms:
+                f['form_name']='<div class="%s"><div class="spn-act-priority">%s</div></div>'%(f['priority_class'],f['form_name'])
+                link=os.path.join(cfg.host,'project',str(cfg.project_factor*int(f['project_id'])),str(f['form_id']))
+                f['link']='<a href="%s" target="_blank"><i class="fa fa-external-link"></i></a>'%link
+
+            response['data']=forms
+            response['success']=True
+            response['recordsTotal']=form_count[0]['count']
+            response['recordsFiltered']=form_count[0]['count']
+
+        else:
+            response['success']=False
+            response['msg_response']='Ocurrió un error al intentar procesar la información, favor de intentarlo de nuevo.'
+    except:
+        response['success']=False
+        response['msg_response']='Ocurrió un error, favor de intentarlo de nuevo.'
+        exc_info=sys.exc_info()
+        app.logger.info(traceback.format_exc(exc_info))
+        GF.sendErrorMail(traceback.format_exc(exc_info))
+    return json.dumps(response)
